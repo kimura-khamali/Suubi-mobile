8635164cfda9b89d664e43b7f7a3e9d4248ee921
Valentine-Nkatha <valentinnenkatha@gmail.com>
2024-07-07 17:17:16 +0300

    first commit

--- app/src/main/java/com/akirachix/suubi_solution/MainActivity.kt
+++ app/src/main/java/com/akirachix/suubi_solution/MainActivity.kt
@@ -6,20 +6,33 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.akirachix.suubi_solution.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding=ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        setContentView(binding.root)
+        binding.btnNext.setOnClickListener {
+            val intent = Intent(this,homeActivity::class.java)
         }
     }
 }
-
+//lateinit var binding:ActivityMainBinding
+//override fun onCreate(savedInstanceState: Bundle?) {
+//    binding=ActivityMainBinding.inflate(layoutInflater)
+//    super.onCreate(savedInstanceState)
+//
+//    setContentView(binding.root)
+//    binding.btnNext.setOnClickListener {
+//        val intent = Intent(this, second_bird::class.java)
+//        startActivity(intent)
+//    }
+//
+//}
+//}
 
 //
 //binding.textView2
